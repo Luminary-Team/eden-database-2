369ea0b296bb6ecec88846e85f8c39cffcb80598
MateusGOliveira <mateus.gomes@germinare.org.br>
2024-10-31 11:29:06 -0300

    Subindo procedures

---
+++ procedures.sql
@@ -0,0 +1,65 @@
+
+
+CREATE OR REPLACE PROCEDURE total_sale_calc(cart_id INTEGER)
+LANGUAGE 'plpgsql'
+AS $$
+BEGIN
+    -- Verifica se existem itens no carrinho
+    IF EXISTS (SELECT 1 FROM carts_itens WHERE fk_carts_id = cart_id) THEN
+        -- Atualiza o total da venda com a soma dos preços dos produtos
+        UPDATE carts
+        SET total_sale = (
+            SELECT SUM(p.price) 
+            FROM products p
+            JOIN carts_itens ci ON ci.fk_product_id = p.pk_id
+            WHERE ci.fk_carts_id = cart_id
+        )
+        WHERE pk_id = cart_id;
+    ELSE
+        -- Se não houver itens no carrinho, define o total como 0
+        UPDATE carts
+        SET total_sale = 0
+        WHERE pk_id = cart_id;
+    END IF;
+END;
+$$;
+
+
+
+CREATE OR REPLACE PROCEDURE user_rating_calc(user_id INTEGER)
+LANGUAGE 'plpgsql'
+AS $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM ratings WHERE fk_user_appraised_id = user_id) THEN
+        UPDATE users
+        SET rating = (SELECT AVG(rating) FROM ratings WHERE fk_user_appraised_id = user_id)
+        WHERE pk_id = user_id;
+    END IF;
+
+END;
+$$;
+
+
+CREATE OR REPLACE PROCEDURE product_rating_calc(product_id INTEGER)
+LANGUAGE 'plpgsql'
+AS $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM ratings WHERE fk_product_id = product_id) THEN
+        UPDATE products
+        SET rating = (SELECT AVG(rating) FROM ratings WHERE fk_product_id = product_id)
+        WHERE pk_id = product_id;
+    END IF;
+
+    -- Commit para transações dentro do procedure
+    COMMIT;
+END;
+$$;
+
+
+
+
+
+
+
+
+
